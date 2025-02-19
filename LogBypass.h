75be83e569f9951d82cdf9e017b4186c03ceabec
t0ughknuckles <103986726+t0ughknuckles@users.noreply.github.com>
2025-02-19 17:38:48 -0500

    Update LogBypass.h

--- LogBypass.h
+++ LogBypass.h
@@ -15,39 +15,26 @@ const char** __fastcall Hook(const char** a1, const char* a2, ...)
     va_list args;
     va_start(args, a2);
 
-    if (strstr(a2, "https://crash-ingress.fivem.net")) 
+    if (strstr(a2, "https://crash-ingress.fivem.net"))
         a2 = "https://google.com/";
 
     const char* modified_args[] = { a2, va_arg(args, const char*) };
     const char** result = function(a1, modified_args[0], modified_args[1]);
+
     va_end(args);
     return result;
 }
 
-DWORD WINAPI InitLogBypass() 
+DWORD WINAPI InitLogBypass(LPVOID)
 {
-    DWORD old;
-    if (!VirtualProtect(function, 1, PAGE_EXECUTE_READWRITE, &old))
-    {
-        MessageBoxA(0, "failed to change mem protection", "err", MB_OK | MB_ICONERROR);
-        return 1;
-    }
+    DWORD oldProtect;
+    if (!VirtualProtect(function, 1, PAGE_EXECUTE_READWRITE, &oldProtect)) { return 1; }
 
     DetourTransactionBegin();
     DetourUpdateThread(GetCurrentThread());
 
-    if (DetourAttach(&(PVOID&)function, Hook) != NO_ERROR)
-    {
-        MessageBoxA(0, "failed to hook function", "err", MB_OK | MB_ICONERROR);
-        DetourTransactionAbort();
-        return 1;
-    }
-
-    if (DetourTransactionCommit() != NO_ERROR) 
-    {
-        MessageBoxA(0, "failed to commit hook", "err", MB_OK | MB_ICONERROR);
-        return 1;
-    }
+    if (DetourAttach(&(PVOID&)function, Hook) != NO_ERROR) { DetourTransactionAbort(); return 1; }
+    if (DetourTransactionCommit() != NO_ERROR) { return 1; }
 
     return 0;
 }
